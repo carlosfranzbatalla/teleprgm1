0217ac38ae72b3f9d586b52888e4d0ef9715cbea
cfbatalla <carlosfranzbatalla@gmail.com>
2022-03-11 21:56:00 -0400

    se agrega var

--- CLASES/CLASE01/Codigo/holaProgramacion1.py
+++ CLASES/CLASE01/Codigo/holaProgramacion1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-from flask_security import ForgotPasswordForm
-
-
 vMiNombre = "Carlos F. Batalla V."
-print("Hola Prog1, soy ", vMiNombre)
+vEdad = 21
+
+print("Hola Prog1, soy ", vMiNombre, "y mi edad es:", str(vEdad))
